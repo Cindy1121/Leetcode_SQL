cc0bb4f834e75b9834f6e66e58a0c68fec60ae1a
Cindy1121 <cindy.chenyang@hotmail.com>
2019-03-21 14:23:38 -0400

    Create 626. Exchange Seats.sql

---
+++ 626. Exchange Seats.sql
@@ -0,0 +1,32 @@
+# Write your MySQL query statement below
+Select 
+    (case when (Select max(id) from seat)%2=1 
+                and 
+                id = (select max(id) from seat) 
+          then id
+          when id%2 = 1
+          then id+1
+          else id-1
+     end) as id, student
+From seat
+Order by id
+
+
+# Different indentation
+Select 
+     (case 	
+     		    when(
+     			 	        Select max(id) from seat
+					          )%2=1 
+                and 
+               	 	  id = (
+               	 		  select max(id) from seat
+               	 	  ) 
+          		  then id
+          	when id%2 = 1
+          		  then id+1
+            else id-1
+     end
+    )as id, student
+From seat
+Order by id
